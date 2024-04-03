1de2e2b4dbbd5b5c81a1952d5ee8ac53ebbfe21e
Niket-mishra <niketmishraofficial@gmail.com>
2024-04-03 10:52:38 +0530

    Updated Website

--- custom.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: [" Full Stack Developer. ", ' Youtuber. ', ' Freelancer. '],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true,
-  });
